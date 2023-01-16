e4b7fedb15f5f4cc04f1ab7f4fd8aca300db46f7
Алексей Тапехин <Aleksey.Tapekhin@vittepro.ru>
2023-01-16 10:19:43 +0300

    Добавьте файлы проекта.

---
+++ Density/DataBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Density
+{
+    class DataBase
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=VLG-5CD5491NCN; Initial Catalog=OperationalRegistrationTest;Integrated Security=True");
+
+        public void OpenConnection()
+        {
+            if (con.State == System.Data.ConnectionState.Closed) con.Open();
+
+        }
+        public void CloseConnection()
+        {
+            if (con.State == System.Data.ConnectionState.Open) con.Close();
+
+        }
+
+        public SqlConnection GetConnection()
+        {
+            return con;
+        }
+              
+
+    }
+}
---
+++ Density/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Density
+{
+    public partial class Form1 : Form
+    {
+        DataBase bd = new DataBase();
+        public Form1()
+        {
+          InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           SqlDataAdapter adapter= new SqlDataAdapter(); 
+            DataTable table= new DataTable();
+            string qsql = "SELECT *  FROM [OperationalRegistrationTest].[dbo].[Standard3900Density]";
+            SqlCommand command = new SqlCommand(qsql, bd.GetConnection());
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+
+            if(table.Rows.Count > 0)
+            {
+                MessageBox.Show("Зашёл","jr", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            MessageBox.Show("Закрываем соединение", "jr", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            bd.CloseConnection();
+        }
+    }
+}
